ac2293aaa101e017fba5995b276931964a284b3d
Lautaro Agustín Agüero <lautiiag179@gmail.com>
2026-08-27 16:12:23 -0300

    Limpieza

--- IntegracionPractica/src/main/java/Ejercicio_05/Main.java
+++
@@ -1,4 +0,0 @@
-package Ejercicio_05;
-
-public class Main {
-}
--- IntegracionPractica/src/main/java/Ejercicio_06/Herbivoro.java
+++
@@ -1,7 +0,0 @@
-package Ejercicio_06;
-
-public class Herbivoro extends EspecieAnimal {
-    public Herbivoro(String nombreCientifico, String nombreVulgar, String periodoCelo) {
-        super(nombreCientifico, nombreVulgar, periodoCelo);
-    }
-}
